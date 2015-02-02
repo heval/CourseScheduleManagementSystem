745b6b0e75d296ac3281b4dbcae2a7dc05073ba6
HevalBerkNevruz <hevalberknevruz@gmail.com>
2015-02-03 00:53:08 +0200

    Singleton Pattern

--- src/org/managementsystem/model/HibernateUtil.java
+++ src/org/managementsystem/model/HibernateUtil.java
@@ -1,31 +1,27 @@
 package org.managementsystem.model;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
-@SuppressWarnings("deprecation")
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static SessionFactory sessionFactory = null;
+	private static StandardServiceRegistry serviceRegistry = null;
 
-	private static SessionFactory buildSessionFactory() {
-		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			return new AnnotationConfiguration().configure()
-					.buildSessionFactory();
-
-		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
+	private synchronized static void buildSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 		}
 	}
 
 	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			buildSessionFactory();
+		}
 		return sessionFactory;
-	}
-
-	public static void shutdown() {
-		// Close caches and connection pools
-		getSessionFactory().close();
 	}
 }
